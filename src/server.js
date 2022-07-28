83d0c4d10aa17bda815957998b86e2fd648b1d1a
Lee <icemokacat@gmail.com>
2022-07-29 00:00:49 +0900

    pug 템플릿 이용한 페이지 연결
    페이지 연결 및 router 설정

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import path from 'path';
+const __dirname = path.resolve();   // common 모듈이 아닌 es 모듈사용시 import 시켜줘야함
 
 const app = express();
 
-console.log('hello');
+console.log(__dirname)
 
-app.listen(3000);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+
+app.use("/public", express.static(__dirname + "/src/public"));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log(`Listen on http://localhost:3000`);
+
+app.listen(3000,handleListen);
