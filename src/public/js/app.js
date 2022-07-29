ea8dab1ee2068aebd9319c5eacbad079d96d3ee0
Lee <icemokacat@gmail.com>
2022-07-29 17:14:42 +0900

    1. 웹소켓 라이브러리 버전 변경 2. 서버 클라이언트간 say hello 웹소켓 작성

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,20 @@
-alert("hi");
+
+const url       = window.location.host;
+const socket    = new WebSocket(`ws://${url}`);
+
+// message : event
+socket.addEventListener("open",() =>{
+    console.log("Connected to Browser ❤️");
+});
+
+socket.addEventListener("message",(message)=>{
+    console.info("New message  :",message.data);
+});
+
+socket.addEventListener("close",()=>{
+    console.log("Disconnected Server 💔");
+});
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+},10000);
--- src/server.js
+++ src/server.js
@@ -1,17 +1,39 @@
-import express from "express";
+import express, { response } from "express";
+import http from "http";
+import WebSocket from "ws";
 import path from 'path';
-const __dirname = path.resolve();   // common 모듈이 아닌 es 모듈사용시 import 시켜줘야함
 
+const __dirname = path.resolve();   // common 모듈이 아닌 es 모듈사용시 import 시켜줘야함
 const app = express();
 
-console.log(__dirname)
-
+// view 단을 설정
 app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views");
 
+// resources 경로 지정
 app.use("/public", express.static(__dirname + "/src/public"));
-app.get("/", (req, res) => res.render("home"));
 
-const handleListen = () => console.log(`Listen on http://localhost:3000`);
+// router
+app.get("/" , (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-app.listen(3000,handleListen);
+// server 세팅
+const port = 3000;
+const handleListen = () => console.log(`Listen on http://localhost:`+port);
+// app.listen(3000,handleListen); 
+
+const server    = http.createServer(app);
+const wss       = new WebSocket.Server({server});
+
+// connection eventListner
+wss.on("connection",(socket) => {
+    console.log("Connected to Browser ✅");
+    // client 연결 끊기면 실행 (여기서는 browser)
+    socket.on("close",() => console.log("Disconnected from the Browser ❎") );
+    socket.on("message", message => {
+        console.log("from browser msg : "+message);
+    });
+    socket.send("Hello socket!");
+});
+
+server.listen(port,handleListen);
