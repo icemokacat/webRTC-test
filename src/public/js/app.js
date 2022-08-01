e595f6fbbf84325e84063947507960661b01d71b
Lee <icemokacat@gmail.com>
2022-08-01 13:35:46 +0900

    1. websocket to socket.io 2. fontend 에서 socket으로 callback func 전달

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,52 +1,23 @@
+const socket = io();
 
-const messageList   = document.querySelector("ul");
-const nickForm      = document.querySelector("#nickname");
-const messageForm   = document.querySelector("#message");
+const welcome = document.getElementById("welcome")
+const form = welcome.querySelector("form")
 
-const url       = window.location.host;
-const socket    = new WebSocket(`ws://${url}`);
-
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
+// functions
+function backendDone(msg){
+    console.log("The backend says:"+msg);
 }
 
-function handleOpen(){
-    console.log("Connected to Browser ❤️");
-}
-
-
-// message : event
-socket.addEventListener("open",handleOpen);
-
-socket.addEventListener("message", (message) => {
-    const li = document.createElement("li");
-    li.innerText = message.data; 
-    messageList.append(li);
-});
-
-socket.addEventListener("close",onSocketClose);
-
-// json to string
-function makeMessage(type,payload){
-    const msg = {type,payload}
-    return JSON.stringify(msg);
-}
-
-// 메세지
-function handleSubmit(event) {
+function handleRoomSubmit(event){
     event.preventDefault();
-    const input = messageForm.querySelector("input");
-    socket.send(makeMessage("new_message",input.value));
+    const input = form.querySelector("input");
+    // websocket의 send
+    socket.emit("enter_room",input.value,
+        // callback func to back end
+        // 마지막은 callback func 임
+        backendDone
+    );
     input.value = "";
-    
 }
 
-// 닉네임
-function handleNickSubmit(event){
-    event.preventDefault();
-    const input = nickForm.querySelector("input");
-    socket.send(makeMessage("nickname",input.value));
-}
-
-messageForm.addEventListener("submit",handleSubmit);
-nickForm.addEventListener("submit",handleNickSubmit);
+form.addEventListener("submit",handleRoomSubmit)
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express, { json, response } from "express";
 import http from "http";
-import WebSocket from "ws";
+import {Server} from "socket.io";
+//import WebSocket from "ws";
 import path from 'path';
+import { doesNotThrow } from "assert";
 
 const __dirname = path.resolve();   // common 모듈이 아닌 es 모듈사용시 import 시켜줘야함
 const app = express();
@@ -20,57 +22,66 @@ app.get("/*", (_, res) => res.redirect("/"));
 // server 세팅
 const port = 3000;
 const handleListen = () => console.log(`Listen on http://localhost:`+port);
-// app.listen(3000,handleListen); 
 
-const server    = http.createServer(app);
-const wss       = new WebSocket.Server({server});
+const httpServer    = http.createServer(app);
+const wsServer      = new Server(httpServer);
 
-function onSocketClose() {
-    console.log("Disconnected from the Browser ❌");
-}
+wsServer.on("connection", socket => {
+    // 여러개의 파라미터 전송 가능
+    // 마지막은 callback func 임
+    socket.on("enter_room", (roomName,done) => {
+        console.log(roomName);
+        setTimeout(()=>{
+            done("hello from the backend");
+        },10000)
+    });
+})
 
-function onSocketMessage(message) {
-    console.log(message);
-}
+// function onSocketClose() {
+//     console.log("Disconnected from the Browser ❌");
+// }
 
-function getPayload(json){
-   const payload = json['payload'];
-   return payload; 
-}
+// function onSocketMessage(message) {
+//     console.log(message);
+// }
 
-function getType(json){
-    const type = json['type'];
-    return type;
-}
+// function getPayload(json){
+//    const payload = json['payload'];
+//    return payload; 
+// }
 
-const sockets   = [];
-const nickNames = [];
+// function getType(json){
+//     const type = json['type'];
+//     return type;
+// }
+
+//const sockets   = [];
 
 // connection eventListner
-wss.on("connection",(socket) => {
-    sockets.push(socket);
-    socket["nickname"] = "Anos";
-    console.log("Connected to Browser ✅");
-    // client 연결 끊기면 실행 (여기서는 browser)
-    socket.on("close"   ,onSocketClose );
-    socket.on("message" , (msg) => {
-        const message   = JSON.parse(msg);
-        const type      = getType(message);
-        const payload   = getPayload(message);
+// wss.on("connection",(socket) => {
+//     sockets.push(socket);
+//     socket["nickname"] = "Anos";
+//     console.log("Connected to Browser ✅");
+//     // client 연결 끊기면 실행 (여기서는 browser)
+//     socket.on("close"   ,onSocketClose );
+//     socket.on("message" , (msg) => {
+//         const message   = JSON.parse(msg);
+//         const type      = getType(message);
+//         const payload   = getPayload(message);
 
-        switch (type) {
-            case 'new_message':
-                // 메세지일때
-                sockets.forEach((aSocket) => {
-                    aSocket.send(`[${socket.nickname}] : ${payload}`);
-                })
-            case 'nickname':
-                // 닉네임일때
-                nickNames.push(payload);
-                socket["nickname"] = message.payload;
-        }
+//         switch (type) {
+//             case 'new_message':
+//                 // 메세지일때
+//                 sockets.forEach((aSocket) => {
+//                     aSocket.send(`[${socket.nickname}] : ${payload}`);
+//                 })
+//             case 'nickname':
+//                 // 닉네임일때
+//                 nickNames.push(payload);
+//                 socket["nickname"] = message.payload;
+//         }
         
-    });
-});
+//     });
+// });
 
-server.listen(port,handleListen);
+httpServer.listen(port,handleListen);
